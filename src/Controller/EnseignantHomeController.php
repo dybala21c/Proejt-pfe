0e4d68c3a7b3f9891a527391982b0df70a181bd7
conde <condemamadylamine@gmail.com>
2020-07-21 00:37:37 +0100

    ajout de l'annonce de l'enseignant

--- src/Controller/EnseignantHomeController.php
+++ src/Controller/EnseignantHomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EnseignantHomeController extends AbstractController
@@ -10,10 +14,33 @@ class EnseignantHomeController extends AbstractController
     /**
      * @Route("/enseignant/home", name="enseignant_home")
      */
-    public function index()
+    public function index(Request $request, PaginatorInterface $paginatorInterface)
     {
+        $annonce_enseignant = new Annonce();
+
+        $donnee = $this->getDoctrine()->getRepository(Annonce::class)->findAll();
+
+        $annonce_enseignant = $paginatorInterface->paginate(
+            $donnee,
+            $request->query->getInt('page',1),
+            5
+        );
         return $this->render('enseignant_home/index.html.twig', [
-            'controller_name' => 'EnseignantHomeController',
+            'annonce_enseignants'=> $annonce_enseignant,
         ]);
     }
+
+    
+    /**
+     * @Route("/{id}/enseignant/annonce", name="annonce_enseignant_show", methods={"GET"})
+     */
+    public function show(Annonce $annonce_enseignant): Response
+    {
+        return $this->render('enseignant_home/show.html.twig', [
+            'annonce' => $annonce_enseignant,
+        ]);
+    }
+
+
+
 }
